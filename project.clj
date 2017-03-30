59cb7c9e43364913bb54286a0d6d59e77d1da00a
Aphyr <aphyr@aphyr.com>
2017-03-30 11:11:07 -0500

    1.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dom-top "1.0.0"
+(defproject dom-top "1.0.1"
   :description "Unorthodox control flow"
   :url "http://github.com/aphyr/dom-top"
   :license {:name "Eclipse Public License"
