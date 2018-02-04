c18c231d5053808fa4dfc25f1960cac602b25829
Aphyr <aphyr@aphyr.com>
2018-02-04 11:57:11 -0600

    Clojure 1.9.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject dom-top "1.0.1"
+(defproject dom-top "1.0.2-SNAPSHOT"
   :description "Unorthodox control flow"
   :url "http://github.com/aphyr/dom-top"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.8.0"]]}})
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]}})
