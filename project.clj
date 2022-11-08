693552726fcc97c9c8af6e5bfe08811b33bc2f30
Aphyr <aphyr@aphyr.com>
2022-11-07 21:32:20 -0500

    mutable-acc-type: Replace insn with direct clojure.asm

--- project.clj
+++ project.clj
@@ -3,12 +3,7 @@
   :url "http://github.com/aphyr/dom-top"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[riddley "0.2.0"] ; For code-walking
-                 ; For ASM generation. We're pulling in an old version so it
-                 ; doesn't conflict with core.async. Since the ONLY thing we
-                 ; need this for is generating simple classes with no methods,
-                 ; might be worth dropping this and using clojure.asm instead.
-                 [insn "0.2.1"]]
+  :dependencies [[riddley "0.2.0"]] ; For code-walking
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.11.1"]
                                   [org.clj-commons/primitive-math "1.0.0"]
                                   [criterium "0.4.6"]]}}
--- src/dom_top/core.clj
+++ src/dom_top/core.clj
@@ -3,10 +3,16 @@
   (:require [clojure [pprint :refer [pprint]]
                      [string :as str]
                      [walk :as walk]]
-            [insn.core :as insn]
             [riddley.walk :refer [macroexpand-all walk-exprs]])
   (:import (java.lang Iterable)
-           (java.util Iterator)))
+           (java.util Iterator)
+           ; oh no
+           (clojure.asm ClassVisitor
+                        ClassWriter
+                        Opcodes
+                        Type)
+           (clojure.lang DynamicClassLoader
+                         RT)))
 
 (defmacro assert+
   "Like Clojure assert, but throws customizable exceptions (by default,
@@ -857,42 +863,71 @@
   mutable-acc-cache*
   (atom {}))
 
+(defn type->desc
+  "Takes a type (e.g. 'int, 'objects, 'longs, 'Foo) and converts it to a JVM
+  type descriptor like \"I\"."
+  [t]
+  (.getDescriptor
+    (case t
+      byte          Type/BYTE_TYPE
+      short         Type/SHORT_TYPE
+      int           Type/INT_TYPE
+      long          Type/LONG_TYPE
+      float         Type/FLOAT_TYPE
+      double        Type/DOUBLE_TYPE
+      bytes         (Type/getType "[B")
+      shorts        (Type/getType "[S")
+      ints          (Type/getType "[I")
+      longs         (Type/getType "[J")
+      floats        (Type/getType "[F")
+      doubles       (Type/getType "[D")
+      objects       (Type/getType "[Ljava/lang/Object;")
+      ; Everything else is an Object for us
+      (Type/getType Object))))
+
 (defn mutable-acc-type
   "Takes a list of types as symbols and returns the class of a mutable
   accumulator which can store those types. May compile new classes on the fly,
   or re-use a cached class.
 
-  TODO: someone clever-er than I please replace the use of insn here with plain
-  old clojure.asm. I'm sure it can be done, I'm just not clever enough to
-  figure it out so far."
+  This method largely courtesy of Justin Conklin! *hat tip*"
   [types]
   (let [; All objects are the same as far as we're concerned.
         types (mapv (fn [type]
                       (if (and type (re-find #"^[a-z]" (name type)))
-                        (keyword type)
+                        type
                         'Object))
                     types)]
     (or (get @mutable-acc-cache* types)
         (let [class-name (str "dom-top.core.MutableAcc-"
                               (str/join "-" (map name types)))
-              klass (insn/define
-                      {:name (symbol class-name)
-                       :fields (map-indexed
-                                 (fn [i type]
-                                   {:flags #{:public}
-                                    :name  (str "x" i)
-                                    :type  (case type
-                                             ; Valid Clojure type hints which
-                                             ; we have to transform into string
-                                             ; descriptors. Type-wise arrays
-                                             ; are all objects, soooo...
-                                             (:bytes :shorts :ints :longs
-                                                     :floats :doubles :objects)
-                                             Object
-                                             type)})
-                                 types)})]
-          (swap! mutable-acc-cache* assoc types klass)
-          klass))))
+              base-type "java/lang/Object"
+              ; Construct class bytecode
+              cv (doto (ClassWriter. ClassWriter/COMPUTE_FRAMES)
+                   (.visit Opcodes/V1_7
+                           (bit-or Opcodes/ACC_PUBLIC Opcodes/ACC_FINAL)
+                           (.replace class-name \. \/)
+                           nil base-type nil))]
+          ; Constructor
+          (doto (.visitMethod cv Opcodes/ACC_PUBLIC "<init>" "()V" nil nil)
+            (.visitCode)
+            (.visitVarInsn Opcodes/ALOAD 0)
+            ; Super
+            (.visitMethodInsn Opcodes/INVOKESPECIAL base-type
+                              "<init>" "()V" false)
+            (.visitInsn Opcodes/RETURN)
+            (.visitMaxs -1 -1)
+            (.visitEnd))
+          ; Fields
+          (doseq [[i t] (map vector (range) types)]
+            (doto (.visitField cv Opcodes/ACC_PUBLIC (str "x" i)
+                               (type->desc t) nil nil)
+              (.visitEnd)))
+          ; And load
+          (let [klass (-> (RT/makeClassLoader)
+                          (.defineClass class-name (.toByteArray cv) nil))]
+            (swap! mutable-acc-cache* assoc types klass)
+            klass)))))
 
 (defmacro reducer
   "Syntactic sugar for writing reducing/transducing functions with multiple
