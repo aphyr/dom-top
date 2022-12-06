47f922e0f7cbc2c2c0352f97a4dbf574d06555a1
Aphyr <aphyr@aphyr.com>
2022-12-05 20:49:40 -0500

    Version 1.0.8

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dom-top "1.0.8-SNAPSHOT"
+(defproject dom-top "1.0.8"
   :description "Unorthodox control flow for Clojurists with masochistic sensibilities"
   :url "http://github.com/aphyr/dom-top"
   :license {:name "Eclipse Public License"
