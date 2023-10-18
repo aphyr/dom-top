52e0a74274c229a18617ebff356902e5140e37e2
Aphyr <aphyr@aphyr.com>
2023-10-18 12:30:37 -0500

    Primitive-math 1.0.1

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject dom-top "1.0.9-SNAPSHOT"
+(defproject dom-top "1.0.9"
   :description "Unorthodox control flow for Clojurists with masochistic sensibilities"
   :url "http://github.com/aphyr/dom-top"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[riddley "0.2.0"]] ; For code-walking
   :profiles {:dev {:dependencies [[org.clojure/clojure "1.11.1"]
-                                  [org.clj-commons/primitive-math "1.0.0"]
+                                  [org.clj-commons/primitive-math "1.0.1"]
                                   [criterium "0.4.6"]]}}
   :test-selectors {:perf :perf
                    :focus :focus
